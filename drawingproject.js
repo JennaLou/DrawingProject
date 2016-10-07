3a9a703e165f636bd8c357be5724a5a3db8ad30c
Jenna Lou <jennalouronquillo@gmail.com>
2016-10-07 11:01:41 -0700

    positioning the eyes

--- drawingproject.js
+++ drawingproject.js
@@ -2,27 +2,32 @@ var canvasDiv = document.getElementById("drawingArea");
 var canvas = canvasDiv.getContext('2d');
 
 
-canvas.fillStyle = "pink";
+canvas.fillStyle = "yellow";
 canvas.beginPath();
 canvas.arc(700, 320, 320,320,2*Math.PI,true);
 canvas.fill();
 
 canvas.beginPath();
-canvas.fillStyle = "black";
-canvas.arc(570,150,40,0,2*Math.PI,true);
+canvas.fillStyle = "white";
+canvas.arc(570,150,50,0,2*Math.PI,true);
+canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "white";
+canvas.arc(800,150,50,0,2*Math.PI,true);
 canvas.fill();
 
 canvas.beginPath();
 canvas.fillStyle = "black";
-canvas.arc(800,150,40,0,2*Math.PI,true);
-canvas.fill();
+canvas.arc(570,150,28,0,2*Math.PI,true);
+canvas.fill()
 
 canvas.beginPath();
-canvas.fillStyle = "pink"
-canvas.arc(400,100,100,0,2*Math.PI,true);
-canvas.fill();
+canvas.fillStyle = "black";
+canvas.arc(800,150,28,0,2*Math.PI,true);
+canvas.fill()
 
 canvas.beginPath();
-canvas.fillStyle = "pink"
-canvas.arc(1000,100,100,0,2*Math.PI,true);
-canvas.fill();
+canvas.fillStyle = "brown";
+canvas.arc(590,300,20,0,2*Math.PI,true);
+canvas.fill()
