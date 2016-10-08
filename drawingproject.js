37d9c2ca6bb38dde8766faacd05640e192753082
Jenna Lou <jennalouronquillo@gmail.com>
2016-10-08 10:45:42 -0700

    changing eye size

--- drawingproject.js
+++ drawingproject.js
@@ -2,32 +2,15 @@ var canvasDiv = document.getElementById("drawingArea");
 var canvas = canvasDiv.getContext('2d');
 
 
-canvas.fillStyle = "yellow";
-canvas.beginPath();
-canvas.arc(700, 320, 320,320,2*Math.PI,true);
-canvas.fill();
+canvas.fillStyle = "#ffff1a";
+canvas.fillRect(400, 150, 500,5000);
 
 canvas.beginPath();
 canvas.fillStyle = "white";
-canvas.arc(570,150,50,0,2*Math.PI,true);
+canvas.arc(570,350,60,0,2*Math.PI,true);
 canvas.fill();
 
 canvas.beginPath();
-canvas.fillStyle = "white";
-canvas.arc(800,150,50,0,2*Math.PI,true);
+canvas.fillStyle = "blue";
+canvas.arc(570,350,35,0,2*Math.PI,true);
 canvas.fill();
-
-canvas.beginPath();
-canvas.fillStyle = "black";
-canvas.arc(570,150,28,0,2*Math.PI,true);
-canvas.fill()
-
-canvas.beginPath();
-canvas.fillStyle = "black";
-canvas.arc(800,150,28,0,2*Math.PI,true);
-canvas.fill()
-
-canvas.beginPath();
-canvas.fillStyle = "brown";
-canvas.arc(590,300,20,0,2*Math.PI,true);
-canvas.fill()
